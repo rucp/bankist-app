0519e745ddd8b19952873952409964143d920a06
rucp <rucp33@gmail.com>
2022-10-04 16:03:58 -0300

    continuing the project: calc and displayng the balance

--- script.js
+++ script.js
@@ -81,6 +81,12 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, curr) => acc + curr, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
@@ -91,5 +97,3 @@ const createUsernames = function (accs) {
   });
 };
 createUsernames(accounts);
-
-console.log(accounts);
